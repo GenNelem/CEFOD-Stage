d713f4fa13e538b26deabc796fad7e30a4704683
Nelem Genevieve <genevieveve.nelem@gmail.com>
2024-03-04 21:46:12 +0100

    changes

--- frontend/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.burger').addEventListener('click', () => {
-    document.querySelector('.menu').classList.toggle('menu-active');
-    document.querySelectorAll('.line').forEach(line => {
-        line.classList.toggle('active');
-    });
-});
